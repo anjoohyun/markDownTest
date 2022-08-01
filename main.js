9d43c8bded3d5b636f172b2a2c9a03537f435f5d
myclick <click7@naver.com>
2022-08-01 14:39:54 +0900

    expreg update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const str = `
-010-123-45676
+010-123-4567
 thetest@test.com
 https://omdgapi.com/?apikey=7035c60&s=frozen
-The quick brown fox jmps over the lazy dog.com
+he quick brown fox jumps over the lazy dog.
 abbbccdddddeffffeg
 `
-//const regexp = new RegExp('the','gi')
-const regexp = /the/gi
-console.log(str.match(regexp))
-alert('test')
+
+console.log(
+    str.match(/(?<=@).{1,}/g)
+)
